5f5e1828ae4686412f951e2191e4a7fd7e74b66a
nnamss <10407499+nnamss@users.noreply.github.com>
2022-05-31 03:34:12 -0400

    backup

---
+++ Assets/Entities/Location.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Location : MonoBehaviour {
+    public void Start() {
+        
+    }
+
+    public void Update() {
+        
+    }
+}
--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Map : MonoBehaviour {
+    private Location[,] gridArray;
+    public string Name = "yoo";
 
     // Start is called before the first frame update
     void Start() {
         Grid grid = new Grid(4, 2, 50f);
     }
-    
+
+    public void SetLocation(int x, int z, Location location) {
+        gridArray[x, z] = location;
+    }
 }
