22bcb2a51c775e0e6647b48eb890a4df2d26ab8d
CryptoWingnut <cryptowingnut@gmail.com>
2022-06-10 15:03:19 -0300

    Add basic mouse handling on cards and a preview window when mousing over one

--- Assets/Scripts/Levels/LevelController.cs
+++ Assets/Scripts/Levels/LevelController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour {
     [field: SerializeField] public LevelId Id;
@@ -9,16 +10,26 @@ public class LevelController : MonoBehaviour {
     protected DeckController _deckController;
 
     protected GameObject _handPosition;
+    private GameObject _cardPreview;
     
     // Start is called before the first frame update
     public void Start() {
         _gameController = GameObject.Find("GameController").GetComponent<GameController>();
         _deckController = GameObject.Find("Player").GetComponent<DeckController>();
         _handPosition = GameObject.Find("HandPosition");
+        _cardPreview = GameObject.Find("CardPreview");
+        if (_cardPreview != null) _cardPreview.SetActive(false);
     }
 
     // Update is called once per frame
     public void Update() {
         
+    }
+
+    public void SetCardPreview(Sprite sprite) {
+        if (_cardPreview == null) return;
+        var image = _cardPreview.GetComponent<Image>();
+        image.sprite = sprite;
+        _cardPreview.SetActive(sprite != null);
     }
 }
