53d3d972d0ccad71786d7321b5bbdcb85a98b915
CryptoWingnut <cryptowingnut@gmail.com>
2022-06-23 21:49:17 -0300

    Correct build error

--- Assets/Scripts/Map/MapBase.cs
+++ Assets/Scripts/Map/MapBase.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public abstract class MapBase : MonoBehaviour {
